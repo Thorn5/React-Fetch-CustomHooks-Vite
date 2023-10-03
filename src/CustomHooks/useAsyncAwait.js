58bbbd30b1cc037bdcf2d873fae2900fa5b92233
Trygve Horn <trygvehorn@gmail.com>
2023-10-03 15:25:56 +0200

    first commit

---
+++ src/CustomHooks/useAsyncAwait.js
@@ -0,0 +1,57 @@
+// useAsyncAwait.js
+import { useState, useEffect } from "react";
+
+const useAsyncAwait = (url) => {
+  if (typeof url !== "string" || url.length === 0) {
+    throw new Error("Invalid URL");
+  }
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+  const [apiData, setApiData] = useState([]);
+  const moduleCalled = "Data fetched using useAsyncAwait";
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const data = await response.json();
+      setApiData(data);
+    } catch (error) {
+      setError(error.message);
+      console.log({ error });
+    } finally {
+      setLoading(false);
+    }
+    
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  return { loading, error, apiData, moduleCalled };
+};
+
+export default useAsyncAwait;
+
+// Bring to parent module:
+// const url_builder () {
+//   const baseUrl = "https://whatever";
+//   const requestEndpoint = "/end/point/"
+//   const apiKey = "12345";
+//   const requestParams = `?api_key=${apiKey}`;
+//   const url = `${baseUrl}${requestEndpoint}${requestParams}`;
+// }
+// const single_connection () {
+//   const { loading, error, apiData, moduleCalled } = useAsyncAwait(url);
+// }
+// const multiple_connections () {
+//   const { loading: productLoading, error: productError, apiData: productData, moduleCalled: productModuleCalled, } = useAsyncAwait(productUrl);
+//   const { loading: customerLoading, error: customerError, apiData: customerData, moduleCalled: customerModuleCalled, } = useAsyncAwait(customerUrl);
+// }
+// const test_return () {
+//   return (<>{loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log(apiData, moduleCalled)}</div>)}
+// }
---
+++ src/CustomHooks/useFetch.js
@@ -0,0 +1,46 @@
+// useFetch.js
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+  const [apiData, setApiData] = useState(null);
+  const moduleCalled = "Data fetched using useFetch";
+
+  // console.log(url);
+  // console.log(data);
+
+  const fetchData = () => {
+    if (typeof url !== "string" || url.length === 0) {
+      throw new Error("Invalid URL");}
+    setLoading(true)
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setApiData(data))
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  
+  useEffect(() => {
+        fetchData();
+  }, []);
+
+  return { loading, error, apiData, moduleCalled };
+};
+
+export default useFetch;
+
+// Bring to parent component:
+// const baseUrl = "https://whatever";
+// const requestEndpoint = "/end/point/"
+// const apiKey = "12345";
+// const requestParams = `?api_key=${apiKey}`;
+// const url = `${baseUrl}${requestEndpoint}${requestParams}`;
+
+// const { loading, error, apiData, moduleCalled } = useFetch(url);
+
+// return (<>{loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log(apiData, moduleCalled)}</div>)}</>)

ea5b1f095f4382166e5b6f7391ebb6490c1a26b4
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-10-06 11:20:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ import { UseFetchTestServer } from "./TestHookConnections/useFetchTestServer";
 import { UseMockDataTest } from "./TestHookConnections/useMockDataTest";
 
 function App() {
+
+  useEffect(() => {        // * URL API diagnostics
+  console.clear();
+  console.log("%c*+*+*+*+*+*+ API useEffect diagnostics *+*+*+*+*+*+","color: #35e859" );
+  <UseAsyncAwaitTestApiUrl />
+  <UseAsyncAwaitTestServer />
+  <UseFetchTestApiUrl />
+  <UseFetchTestServer />
+  <UseMockDataTest /> 
+  console.log("%c*+*+*+*+*+*+ /API useEffect diagnostics/ *+*+*+*+*+*+", "color: #35e859" );
+  }, []);
+  
   return (
     <>
       <div>
